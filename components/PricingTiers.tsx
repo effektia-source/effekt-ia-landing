686d8e51574e2122db63477b6af2a3c8dd048c71
Víctor Martínez <effektia@gmail.com>
2026-01-30 20:18:41 -0600

    Add anchor id to pricing section

--- components/PricingTiers.tsx
+++ components/PricingTiers.tsx
@@ -5,7 +5,9 @@ import { droneData } from '@/data/droneData';
 
 export default function PricingTiers() {
     return (
-        <section className="py-32 bg-black text-white relative overflow-hidden">
+        <section
+            id="planes"
+            className="py-32 bg-black text-white relative overflow-hidden">
             {/* Background Grid */}
             <div className="absolute inset-0 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] pointer-events-none" />
 
